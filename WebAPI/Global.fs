4ec13b4ba6af4a6def2ed3a3ff9f9fc27754e39f
Curtis Badke <curtis@badkid.ca>
2013-02-24 08:47:29 -0700

    Getting compiling/bundling of less files.

--- WebAPI/Global.fs
+++ WebAPI/Global.fs
@@ -7,10 +7,17 @@ open System.Web.Http
 open System.Web.Routing
 open System.Web.Optimization
 
+type LessTransform() = 
+    interface IBundleTransform with
+        member x.Process (context:BundleContext, response:BundleResponse) =
+            response.Content <- dotless.Core.Less.Parse response.Content
+            response.ContentType <- "text/css"
+
+
 type BundleConfig() =
     static member RegisterBundles (bundles:BundleCollection) =
 
-        bundles.UseCdn <= true |> ignore
+        bundles.UseCdn <- true
 
         let jqueryCdnPath = "http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
         let jqueryUiCdnPath = "http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.0/jquery-ui.min.js"
@@ -20,8 +27,13 @@ type BundleConfig() =
         bundles.Add(ScriptBundle("~/bundles/modernizr").Include("~/Scripts/lib/modernizr-{version}.js"))
         bundles.Add(ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/lib/bootstrap.js"))
 
-        bundles.Add(StyleBundle("~/Content/bootstrap/bundle").Include("~/Content/bootstrap.css", "~/Content/bootstrap-responsive.css"))
-        bundles.Add(StyleBundle("~/Content/jquery-ui/themes/base/bundle").IncludeDirectory("~/Content/themes/base","*.css"))
+        bundles.Add(StyleBundle("~/Content/bootstrap/css").Include("~/Content/bootstrap/bootstrap.css", "~/Content/bootstrap/bootstrap-responsive.css"))
+        bundles.Add(StyleBundle("~/Content/jquery-ui/themes/base/css").IncludeDirectory("~/Content/jquery-ui/themes/base","*.css"))
+
+        let lessBundle = (new Bundle ("~/Content/site-css")).IncludeDirectory("~/Content/less","*.less")
+        lessBundle.Transforms.Add(new LessTransform())
+        lessBundle.Transforms.Add(new CssMinify())
+        bundles.Add(lessBundle)
 
 
 type Route = { controller : string
