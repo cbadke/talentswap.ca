5cd08b0dcd87372f4c3fc4691ba611ec85315c86
Curtis Badke <curtis@badkid.ca>
2013-02-24 19:05:39 -0700

    Add bootstrap CDN refs

--- WebAPI/Global.fs
+++ WebAPI/Global.fs
@@ -19,15 +19,18 @@ type BundleConfig() =
 
         bundles.UseCdn <- true
 
-        let jqueryCdnPath = "http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
-        let jqueryUiCdnPath = "http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.0/jquery-ui.min.js"
+        let jqueryCdnPath = "//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"
+        let jqueryUiCdnPath = "//ajax.googleapis.com/ajax/libs/jqueryui/1.10.0/jquery-ui.min.js"
+
+        let bootstrapCssCdnPath = "//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.0/css/bootstrap-combined.min.css"
+        let bootstrapJsCdnPath = "//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.0/js/bootstrap.min.js"
 
         bundles.Add(ScriptBundle("~/bundles/jquery", jqueryCdnPath).Include("~/Scripts/lib/jquery-{version}.js"))
         bundles.Add(ScriptBundle("~/bundles/jquery-ui", jqueryUiCdnPath).Include("~/Scripts/lib/jquery-ui-{version}.js"))
         bundles.Add(ScriptBundle("~/bundles/modernizr").Include("~/Scripts/lib/modernizr-{version}.js"))
-        bundles.Add(ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/lib/bootstrap.js"))
+        bundles.Add(ScriptBundle("~/bundles/bootstrap", bootstrapJsCdnPath).Include("~/Scripts/lib/bootstrap.js"))
 
-        bundles.Add(StyleBundle("~/Content/bootstrap/css").Include("~/Content/bootstrap/bootstrap.css", "~/Content/bootstrap/bootstrap-responsive.css"))
+        bundles.Add(StyleBundle("~/Content/bootstrap/css", bootstrapCssCdnPath).Include("~/Content/bootstrap/bootstrap.css", "~/Content/bootstrap/bootstrap-responsive.css"))
         bundles.Add(StyleBundle("~/Content/jquery-ui/themes/base/css").IncludeDirectory("~/Content/jquery-ui/themes/base","*.css"))
 
         let lessBundle = (new Bundle ("~/Content/site-css")).IncludeDirectory("~/Content/less","*.less")
